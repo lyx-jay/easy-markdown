7104ea541fa455d7716f40c67b93456c60a273a0
lvyuanxin <1498781418@qq.com>
2024-10-07 13:50:46 +0800

    feat: support batch add backquote

--- src/process-inline-code.ts
+++ src/process-inline-code.ts
@@ -1,78 +1,156 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-const processInlineText = (text: string, type: 'single' | 'batch' = 'batch') => {
-  if (type === 'single') {
-    return '`' + text + '`';
+const processInlineText = (
+  text: string,
+  type: "single" | "batch" = "batch"
+) => {
+  if (type === "single") {
+    return "`" + text + "`";
   }
   let newText = text;
   // 只考虑单词边界情况，同时忽略已有反引号包裹的内容或加粗显示的内容
-  newText = newText.replace(/\b(?<!`)(?<!\*\*)([a-zA-Z0-9_\-.]+)\b(?!`)(?!\*\*)/g, '`$1`');
+  newText = newText.replace(
+    /\b(?<!`)(?<!\*\*)([a-zA-Z0-9_\-.]+)\b(?!`)(?!\*\*)/g,
+    "`$1`"
+  );
   return newText;
 };
 
+export function isTextInComment(text: string, markdownContent: string): boolean {
+  const commentRegex = /<!--([\s\S]*?)-->/g;
+  // console.log('isTextInComment', commentRegex.exec(markdownContent));
+  let match;
+  while ((match = commentRegex.exec(markdownContent))!== null) {
+    if (match[1].includes(text)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+export function isTextInCodeBlock(text: string, markdownContent: string): boolean {
+  const codeBlockRegex = /`([^`]+)`/g;
+  // console.log('isTextInCodeBlock', codeBlockRegex.exec(markdownContent));
+  let match;
+  while ((match = codeBlockRegex.exec(markdownContent))!== null) {
+      if (match[1].includes(text)) {
+          return true;
+      }
+  }
+  return false;
+}
+
+export function isTextStartWithAchor(line: string) {
+  if (line.startsWith("#")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+
 export function registerProcessInlineCodeCommand() {
+  const singleChange = vscode.commands.registerCommand(
+    "easy-markdown.single",
+    () => {
+      // 获取当前打开的编辑窗口
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
 
-  const singleChange = vscode.commands.registerCommand('easy-markdown.single', () => {
-    // 获取当前打开的编辑窗口
-    const editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      return;
-    }
+      // 获取当前打开的文档，只处理 Markdown 文件
+      const document = editor.document;
+      const fileType = document.languageId;
+      if (fileType !== "markdown") {
+        return;
+      }
 
-    // 获取当前打开的文档，只处理 Markdown 文件
-    const document = editor.document;
-    const fileType = document.languageId;
-    if (fileType !== 'markdown') {
-      return;
+      // 如果当前有选中的文本,则只对选中的文本进行处理
+      const selection = editor.selection;
+      const selectedText = document.getText(selection);
+      if (selectedText) {
+        // 进行处理并替换选中的文本
+        const processedText = processInlineText(selectedText, "single");
+        editor.edit((builder) => {
+          builder.replace(selection, processedText);
+        });
+      }
     }
-
-    // 如果当前有选中的文本,则只对选中的文本进行处理
-    const selection = editor.selection;
-    const selectedText = document.getText(selection);
-    if (selectedText) {
-      // 进行处理并替换选中的文本
-      const processedText = processInlineText(selectedText, 'single');
-      editor.edit(builder => {
-        builder.replace(selection, processedText);
-      });
-    }
-  });
+  );
 
   return singleChange;
 }
 
 
+
 export function registerProcessBatchInlineCodeCommand() {
-  const batchingChange = vscode.commands.registerCommand('easy-markdown.batching', () => {
-    // 获取当前打开的编辑窗口
-    const editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      return;
-    }
+  const batchingChange = vscode.commands.registerCommand(
+    "easy-markdown.batching",
+    () => {
+      // 获取当前打开的编辑窗口
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
 
-    // 获取当前打开的文档，只处理 Markdown 文件
-    const document = editor.document;
-    const fileType = document.languageId;
-    if (fileType !== 'markdown') {
-      return;
-    }
+      // 获取当前打开的文档，只处理 Markdown 文件
+      const document = editor.document;
+      const fileType = document.languageId;
+      if (fileType !== "markdown") {
+        return;
+      }
 
-    // 如果当前有选中的文本,则只对选中的文本进行处理
-    const selection = editor.selection;
-    const selectedText = document.getText(selection);
-    if (selectedText) {
-      // 进行处理并替换选中的文本
-      const processedText = processInlineText(selectedText);
-      editor.edit(builder => {
-        builder.replace(selection, processedText);
-      });
-      return;
-    } else {
-      vscode.window.showInformationMessage('暂不支持扫描整个文档，请选择有效的内容进行处理!');
+      // 如果当前有选中的文本,则只对选中的文本进行处理
+      const selection = editor.selection;
+      const selectedText = document.getText(selection);
+      
+      if (selectedText) {
+        // 找到包含有该文本的行
+        const matches = [];
+        for (let i = 0; i < document.lineCount; i++) {
+          const line = document.lineAt(i);
+          if (line.text.includes(selectedText)) {
+            matches.push({ lineNumber: i, text: line.text });
+          }
+        }
+        const allText = document.getText();
+
+        const textMatches = matches.filter(match => {
+          return !isTextInCodeBlock(match.text, allText) && !isTextInComment(match.text, allText) && !isTextStartWithAchor(match.text);
+        });
+
+        const ranges = textMatches.map(match => {
+
+          const text = match.text;
+
+          const startColNum = text.indexOf(selectedText);
+          const endColNum = startColNum + selectedText.length;
+
+          const startPosition = new vscode.Position(match.lineNumber, startColNum);
+          const endPosition = new vscode.Position(match.lineNumber, endColNum);
+          const range = new vscode.Range(startPosition, endPosition);
+
+          return range;
+        });
+
+        editor.edit(builder => {
+          ranges.forEach(range => {
+            const processedText = processInlineText(selectedText);
+            builder.replace(range, processedText);
+          });
+        });
+
+        return;
+      } else {
+        vscode.window.showInformationMessage(
+          "please select the text you want to process"
+        );
+      }
     }
-  });
+  );
 
   return batchingChange;
-
 }
 
